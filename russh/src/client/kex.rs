195830169842eacd28ec943b80780383b7668c66
Georg von Zengen <github@vz-ge.org>
2026-02-14 23:14:10 +0100

    add host certificate support

--- russh/src/client/kex.rs
+++ russh/src/client/kex.rs
@@ -7,17 +7,17 @@ use bytes::Bytes;
 use log::{debug, error, warn};
 use signature::Verifier;
 use ssh_encoding::{Decode, Encode};
-use ssh_key::{Mpint, PublicKey, Signature};
+use ssh_key::{Certificate, Mpint, PublicKey, Signature};
 
 use super::IncomingSshPacket;
 use crate::client::{Config, NewKeys};
 use crate::kex::dh::groups::DhGroup;
-use crate::kex::{KexAlgorithm, KexAlgorithmImplementor, KexCause, KexProgress, KEXES};
+use crate::kex::{KEXES, KexAlgorithm, KexAlgorithmImplementor, KexCause, KexProgress};
 use crate::keys::key::parse_public_key;
 use crate::negotiation::{Names, Select};
 use crate::session::Exchange;
 use crate::sshbuffer::PacketWriter;
-use crate::{msg, negotiation, strict_kex_violation, CryptoVec, Error, SshId};
+use crate::{CryptoVec, Error, SshId, msg, negotiation, strict_kex_violation};
 
 thread_local! {
     static HASH_BUFFER: RefCell<CryptoVec> = RefCell::new(CryptoVec::new());
@@ -120,6 +120,7 @@ impl ClientKex {
                     negotiation::Client::read_kex(
                         &input.buffer,
                         &self.config.preferred,
+                        None,
                         None,
                         &self.cause,
                     )?
@@ -262,19 +263,15 @@ impl ClientKex {
                 #[allow(clippy::indexing_slicing)] // length checked
                 let r = &mut &input.buffer[1..];
 
-                let server_host_key = Bytes::decode(r)?; // server public key.
-                let server_host_key = parse_public_key(&server_host_key)?;
-                debug!(
-                    "received server host key: {:?}",
-                    server_host_key.to_openssh()
-                );
+                let server_host_key_bytes = Bytes::decode(r)?; // server public key.
+                let server_host_key = parse_public_key(&server_host_key_bytes)?;
 
                 let server_ephemeral = Bytes::decode(r)?;
                 self.exchange.server_ephemeral.extend(&server_ephemeral);
                 kex.compute_shared_secret(&self.exchange.server_ephemeral)?;
 
                 let mut pubkey_vec = CryptoVec::new();
-                server_host_key.to_bytes()?.encode(&mut pubkey_vec)?;
+                server_host_key_bytes.encode(&mut pubkey_vec)?;
 
                 let exchange = &self.exchange;
                 let hash = HASH_BUFFER.with({
@@ -288,9 +285,37 @@ impl ClientKex {
                 let signature = Bytes::decode(r)?;
                 let signature = Signature::decode(&mut &signature[..])?;
 
-                if let Err(e) = Verifier::verify(&server_host_key, hash.as_ref(), &signature) {
-                    debug!("wrong server sig: {e:?}");
-                    return Err(Error::WrongServerSig);
+                match Certificate::from_bytes(&server_host_key_bytes) {
+                    // Host certificate verification
+                    Ok(server_certificate) => {
+                        debug!(
+                            "received server certificate: {:?}",
+                            server_certificate.to_openssh()
+                        );
+                        debug!("Parsed server host key as Certificate");
+                        let inner_key_data = server_certificate.public_key().clone();
+                        let inner_public_key = PublicKey::new(inner_key_data, "");
+
+                        if let Err(e) =
+                            Verifier::verify(&inner_public_key, hash.as_ref(), &signature)
+                        {
+                            debug!("Certificate inner key verification failed: {e}");
+                            return Err(Error::WrongServerSig);
+                        }
+                    }
+                    Err(_) => {
+                        // Host key verification
+                        debug!(
+                            "received server host key: {:?}",
+                            server_host_key.to_openssh()
+                        );
+                        if let Err(e) =
+                            Verifier::verify(&server_host_key, hash.as_ref(), &signature)
+                        {
+                            debug!("wrong server sig: {e:?}");
+                            return Err(Error::WrongServerSig);
+                        }
+                    }
                 }
 
                 let newkeys = compute_keys(
